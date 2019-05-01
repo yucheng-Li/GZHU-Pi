525b3d076d4334f95528d84390b73a3b51fdb39d
yangtaihua1 <1548899388@qq.com>
2019-05-01 19:13:33 +0800

    update

--- Server/spider/exam_spider.py
+++ Server/spider/exam_spider.py
@@ -24,11 +24,14 @@ class EX():
             return chTestHandle(res)
     #四六级获取验证码图片
     def cetTestQueryGetImg(self,id_num,name):
-        return get_img(self.client, id_num)
+        imgCookiesData={}
+        imgCookiesData['img']=get_img(self.client, id_num)
+        imgCookiesData['cookies']=requests.utils.dict_from_cookiejar(self.client.cookies)
+        return imgCookiesData
     
     #四六级获取分数
-    def cetTestQueryGetScore(self,id_num,name,capcha):
-        return get_score(self.client, id_num, name,capcha)
+    def cetTestQueryGetScore(self,id_num,name,capcha,cookies):
+        return get_score(self.client, id_num, name,capcha,cookies)
 
     #录取查询
     def admitQuery(self,stuID,stuName):
@@ -57,14 +60,19 @@ testData = {
     'stuID': '',
     'idCard': '***REMOVED***'
 }
-print(test.chTestQuery(testData))'''
-
+print(test.chTestQuery(testData))
+'''
 # #cet考试测试
 # test=EX()
-# print(test.cetTestQueryGetImg('***REMOVED***','肖'))
+# print(test.cetTestQueryGetImg('***REMOVED***','***REMOVED***'))
 # capcha=input()
-# print(test.cetTestQueryGetScore('***REMOVED***','肖',capcha))
-
+'''
+cookiesTest={'BIGipServercache.neea.edu.cn_pool': '2543896586.39455.0000'}
+test1=EX()
+print(test1.cetTestQueryGetScore('***REMOVED***','***REMOVED***',,cookiesTest))
+'''
+'''
 #录取查询
 test=EX()
 test.admitQuery('***REMOVED***','***REMOVED***')
+'''

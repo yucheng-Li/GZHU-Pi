49ca92e36321170cc2bb5cf02001aaa3ad68d40f
ZhenShaw <1614570038@qq.com>
2019-05-03 23:49:49 +0800

    增加全校课程查询

--- Server/spider/exam_spider.py
+++ Server/spider/exam_spider.py
@@ -38,18 +38,18 @@ class Exam(object):
         if ('对不起没有查询到相关信息' in res.text):
             return '对不起没有查询到相关信息'
         else:
-            return chTestHandle(res)
-    #四六级获取验证码图片
-    def cetTestQueryGetImg(self,id_num,name):
-        imgCookiesData={}
-        imgCookiesData['img']=get_img(self.client, id_num)
-        imgCookiesData['cookies']=requests.utils.dict_from_cookiejar(self.client.cookies)
-        return imgCookiesData
-    
-    #四六级获取分数
-    def cetTestQueryGetScore(self,id_num,name,capcha,cookies):
-        return get_score(self.client, id_num, name,capcha,cookies)
+            return ch_test_handler(res)
 
+    # 四六级获取验证码图片
+    def cetTestQueryGetImg(self, id_num, name):
+        imgCookiesData = {}
+        imgCookiesData['img'] = get_img(self.client, id_num)
+        imgCookiesData['cookies'] = requests.utils.dict_from_cookiejar(self.client.cookies)
+        return imgCookiesData
+
+    # 四六级获取分数
+    def cetTestQueryGetScore(self, id_num, name, capcha, cookies):
+        return get_score(self.client, id_num, name, capcha, cookies)
 
     # 四六级获取验证码图片
     def cet_get_captcha(self, id_num, name):
@@ -59,27 +59,27 @@ class Exam(object):
     def cet_get_score(self, id_num, name, capcha):
         return get_score(self.client, id_num, name, capcha)
 
-'''# 普通话考试测试
-test = EX()
+
+# 普通话考试测试
+test = Exam()
 testData = {
     'name': '***REMOVED***',
     'stuID': '',
     'idCard': '***REMOVED***'
 }
-print(test.chTestQuery(testData))
-'''
+print(test.ch_test_query(testData))
+
 # #cet考试测试
 # test=EX()
 # print(test.cetTestQueryGetImg('***REMOVED***','***REMOVED***'))
 # capcha=input()
-'''
-cookiesTest={'BIGipServercache.neea.edu.cn_pool': '2543896586.39455.0000'}
-test1=EX()
-print(test1.cetTestQueryGetScore('***REMOVED***','***REMOVED***',,cookiesTest))
-'''
+
+# cookiesTest={'BIGipServercache.neea.edu.cn_pool': '2543896586.39455.0000'}
+# test1=Exam()
+# print(test1.cetTestQueryGetScore('***REMOVED***','***REMOVED***',cookiesTest))
+
 '''
 #录取查询
 test=EX()
 test.admitQuery('***REMOVED***','***REMOVED***')
 '''
-
